2599b80322e4fad42c2c88d1570681b83a62b2c0
Peter Ivanov <peter@microweber.com>
2015-07-08 16:46:41 +0300

    version

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $config = array();
 $config['name'] = "Bootstrap3";
 $config['author'] = "Microweber";
-$config['version'] = 0.2;
+$config['version'] = 0.3;
 $config['url'] = "http://bootstrap.com";
